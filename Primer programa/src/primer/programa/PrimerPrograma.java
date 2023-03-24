d563bc378b94b926233954e5e6409040f6cde035
Federico Tula <fede.t17@hotmail.com>
2023-03-24 09:30:56 -0300

    Se guarda el segundo archivo de texto

--- Primer programa/src/primer/programa/PrimerPrograma.java
+++ Primer programa/src/primer/programa/PrimerPrograma.java
@@ -3,42 +3,37 @@ package primer.programa;
 import java.util.Scanner;
 
 /**
- *
+ * Codificación de frase con función
  */
 public class PrimerPrograma {
 
-    /**
-     *
+    /*
+    Crea un procedimiento EsMultiplo que reciba los dos números pasados por el usuario, validando que 
+    el primer número múltiplo del segundo e imprima si el primer número es múltiplo del segundo, sino 
+    informe que no lo son.
      */
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int numerosLeidos = 0;
-        int numerosPares = 0;
-        int numerosImpares = 0;
+        int num1,num2;
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese dos números.");
+        num1= leer.nextInt();
+        num2= leer.nextInt();
+        
+        multiplos(num1,num2);
+        
+       /*
+        String retorno = codificacion(frase);
+        System.out.println(retorno);
+*/
+    }
 
-        while (true) {
-            System.out.print("Ingrese un número entero: ");
-            int numero = input.nextInt();
-
-            if (numero < 0) {
-                continue;
-            }
-
-            numerosLeidos++;
-
-            if (numero % 2 == 0) {
-                numerosPares++;
-            } else {
-                numerosImpares++;
-            }
-
-            if (numero % 5 == 0) {
-                break;
-            }
-        }
-
-        System.out.println("Cantidad de números leídos: " + numerosLeidos);
-        System.out.println("Cantidad de números pares: " + numerosPares);
-        System.out.println("Cantidad de números impares: " + numerosImpares);
+    //Subprograma que verifica si son múltiplos los dos números ingresados
+    public static void multiplos(int num1, int num2) {
+    
+if (num1%num2==0) {
+    System.out.println("Los números ingresados son múltiplos");
+} else {
+    System.out.println("Los números ingresados no son múltiplos");
+}
     }
 }
