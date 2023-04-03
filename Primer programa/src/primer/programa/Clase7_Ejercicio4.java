c14df7babace5f3079da02ed17e198422c78c9bd
Federico Tula <fede.t17@hotmail.com>
2023-04-02 21:31:10 -0300

    Se guarda el cuarto archivo

--- Primer programa/src/primer/programa/Clase7_Ejercicio4.java
+++ Primer programa/src/primer/programa/Clase7_Ejercicio4.java
@@ -1,63 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*Clase 10 29/03/2023
  */
 package primer.programa;
 
 import java.util.Scanner;
 
 /**
- *
  * @author Wolf Legendary
  */
 public class Clase7_Ejercicio4 {
 
     /*
-Crea una aplicación que nos pida un número por teclado y con una función se lo pasamos por parámetro 
-para que nos indique si es o no un número primo, debe devolver true si es primo, sino false.
-Un número primo es aquel que solo puede dividirse entre 1 y sí mismo. Por ejemplo: 25 no es primo, ya que 25
-es divisible entre 5, sin embargo, 17 si es primo.
+
      */
     public static void main(String[] args) {
-        Scanner leer = new Scanner(System.in);
-        String respuesta = "Si";//Inicialmente es Si
-        Boolean retorno;
-        do {
-            System.out.println("Ingrese un número, para determinar si es primo.");
-            int num = leer.nextInt();
-
-            leer.nextLine(); //Consumir el salto de línea pendiente
-
-            retorno = verificar(num); //Se llama a la función para determinar si el número es primo
-            System.out.print("La afirmación de que el número " + num + " es primo es ");
-            System.out.println(retorno + ".");
-
-            System.out.println("¿Quiere realizar otra consulta? Si/No");
-            respuesta = leer.nextLine();
-
-        } while (respuesta.equalsIgnoreCase("Si"));
-    }
-
-//Función que dice si el número es primo o no.
-//Un número primo es aquel que solo puede dividirse entre 1 y sí mismo. 
-    public static boolean verificar(int num) {
-        if (num == 1) {
-            return false;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Ingrese un valor entero para N: ");
+        int n = input.nextInt();
+        
+        int[] fibonacci = new int[n];
+        fibonacci[0] = 1;
+        
+        if (n!=1) {
+            fibonacci[1] = 1;
+            for (int i = 2; i < n; i++) {
+            fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
         }
-        int contador = 0;
-        boolean check = true;
-        for (int i = 1; i < num; i++) {
-            if (num % i == 0) {
-                // Suma 1 cuando le da resto cero. Si acumula más de dos casos el numero no es primo.
-                contador++;
-                // Detiene la ejecución si contador es igual a 2.
-                if (contador == 2) {
-                    check = false;
-                    break;
-                }
-            }
         }
-        return check;
+        
+        System.out.println("Los primeros " + n + " números de la sucesión de Fibonacci son:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci[i] + " ");
+        }
     }
 }
+
--- Primer programa/src/primer/programa/PrimerPrograma.java
+++ Primer programa/src/primer/programa/PrimerPrograma.java
@@ -1,43 +1,111 @@
 package primer.programa;
 
+//import java.util.Random;
 import java.util.Scanner;
 
-/**
- * Ejercicio2Clase7
+/*
+Construya un programa que lea 5 palabras de mínimo 3 y hasta 5 caracteres y, a medida que 
+el usuario las va ingresando, construya una “sopa de letras para niños” de tamaño de 20 x 20 caracteres. 
+Las palabras se ubicarán todas en orden horizontal en una fila que será seleccionada de manera aleatoria. 
+Una vez concluida la ubicación de las palabras, rellene los espacios no utilizados con un número aleatorio 
+del 0 al 9. Finalmente imprima por pantalla la sopa de letras creada.
+Nota: Para resolver el ejercicio deberá investigar cómo se utilizan las siguientes funciones de Java substring(), 
+Length() y Math.random().
  */
 public class PrimerPrograma {
-    /*
-Crea una aplicación que a través de una función nos convierta una cantidad de euros introducida 
-por teclado a otra moneda, estas pueden ser a dólares, yenes o libras. La función tendrá como parámetros, 
-la cantidad de euros y la moneda a convertir que será una cadena, este no devolverá ningún valor y mostrará 
-un mensaje indicando el cambio (void).
-El cambio de divisas es:
-* 0.86 libras es un 1 €
-* 1.28611 $ es un 1 €
-* 129.852 yenes es un 1 €
-     */
-    public static void main(String[] args) {
-        Scanner leer = new Scanner(System.in);
-        String respuesta = "Si"; //Inicialmente le damos el valor de Si a respuesta para que se ejecute el bucle  
-  
-        do {
-            System.out.println("Ingrese la cantidad de euros para realizar las conversiones.");
-            
-            int euros = leer.nextInt();
-            leer.nextLine(); //Consumir el salto de línea pendiente
 
-            conversion(euros);
-            
-            System.out.println("¿Quiere realizar otra conversión? Si/No");
-        
-            respuesta=leer.nextLine();
-            
-        } while (respuesta.equalsIgnoreCase("Si"));
+    public static void main(String[] args) {
+        System.out.println("Bienvenido al generador de sopas de letras para niños.");
+        System.out.println("Por favor ingrese 5 palabras de 3 a 5 letras cada una.");
+        Scanner leer = new Scanner(System.in);
+        //Definir una matriz tipo String de 20x20.
+        String sopaDeLetras[][] = new String[20][20];
+        //Pide y lee 5 palabras, con un mínimo de 3 y un máximo de 5 carácteres.
+        String palabra;
+        String[] palabras = new String[5];
+        //Posiciones random
+        int[] posicionFilas = new int[palabras.length];
+        int[] posicionColumnas = new int[palabras.length];
+
+        int contador = 0;
+        do {
+            System.out.println("Ingrese la palabra número " + (contador + 1) + " con un mín. de 3 y un máx. de 5 carácteres.");
+            palabra = leer.nextLine().toUpperCase();
+            if ((palabra.length() < 3 || palabra.length() > 5)) {
+                System.out.println("Ha ingresado una palabra con la cantidad incorrecta de carácteres.");
+            } else {
+
+                palabras[contador] = palabra;
+                contador += 1;
+            }
+        } while (contador < 5);
+
+        muestraVector(palabras);
+        //Generar 5 posiciones random para las  palabras, en filas y columnas.
+        //La posiciones en las filas pueden estar del 0 al 19.
+        posicionFilas = numerosAleatorios(posicionFilas.length, 19, 0);
+        //La posiciones en las columnas pueden estar del 0 al 16.
+        posicionColumnas = numerosAleatorios(posicionColumnas.length, 16, 0);
+
+        //Escribir las palabras dentro de la matriz, en las posiciones generadas.
+        for (int x = 0; x < palabras.length; x++) {
+            sopaDeLetras[posicionFilas[x]][posicionColumnas[x]] = palabras[x];
+        }
+        //Completar valores vacios con números random desde el 0 al 9.
+        for (String[] sopaDeLetra : sopaDeLetras) {
+            for (int j = 0; j < sopaDeLetra.length; j++) {
+                if (null == sopaDeLetra[j]) {
+                    sopaDeLetra[j] = String.valueOf((int) (Math.random() * 10));
+                }
+            }
+        }
+
+        //Mostrar la matriz completa.
+        muestraMatriz(sopaDeLetras);
     }
-    //Subprograma que convierte el Euro en otras monedas
-    public static void conversion(int euros) {
-        System.out.println("El equivalente en libras es " + 0.86 * euros);
-        System.out.println("El equivalente en dólares es " + 1.28611 * euros);
-        System.out.println("El equivalente en yenes es " + 129.852 * euros);
+
+    //Subproceso para mostrar vector
+    public static void muestraVector(String[] vector) {
+        for (String elemento : vector) {
+            System.out.println(elemento);
+        }
+    }
+
+    //Subproceso para mostrar matriz
+    public static void muestraMatriz(String[][] matriz) {
+        for (String[] fila : matriz) {
+            for (String elemento : fila) {
+                //En este caso, el elemento se formatea para tener una longitud de 3 caracteres ("%-3s")
+                String elementoFormateado = String.format("%-3s", elemento);
+                System.out.print(elementoFormateado);
+
+            }
+            System.out.println("");
+        }
+    }
+    //Genera números aleatorios que no se repiten
+
+    public static int[] numerosAleatorios(int cantidad, int maximo, int minimo) {
+        int[] numerosAleatorios = new int[cantidad];
+        for (int i = 0; i < numerosAleatorios.length; i++) {
+            int numeroAleatorio;
+            boolean numeroRepetido;
+
+            do {
+                numeroAleatorio = (int) (Math.random() * (maximo - minimo) + 1);
+                numeroRepetido = false;
+
+                for (int j = 0; j < i; j++) {
+                    if (numerosAleatorios[j] == numeroAleatorio) {
+                        numeroRepetido = true;
+                        break;
+                    }
+                }
+            } while (numeroRepetido);
+
+            numerosAleatorios[i] = numeroAleatorio;
+        }
+
+        return numerosAleatorios;
     }
 }
