e9935b4674f20bdcc2e72c0d17987566e9644450
Federico Tula <fede.t17@hotmail.com>
2023-04-23 22:48:57 -0300

    Se crea guia 9 Ej04

--- Guia9Ej04/src/guia9ej04/Guia9Ej04.java
+++ Guia9Ej04/src/guia9ej04/Guia9Ej04.java
@@ -1,13 +1,20 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Vamos a usar la clase Date que ya existe en Java. Crearemos la clase FechaService, en paquete Servicios, que tenga 
+los siguientes métodos:
+Método fechaNacimiento que pregunte al usuario día, mes y año de su nacimiento. Luego los pase por parámetro a un nuevo 
+objeto Date. El método debe retornar el objeto Date. Ejemplo fecha: Date fecha = new Date(anio, mes, dia);
+Método fechaActual que cree un objeto fecha con el día actual. Para esto usaremos el constructor vacío de la clase Date. Ejemplo: Date fechaActual = new  Date();
+El método debe retornar el objeto Date.
+Método diferencia que reciba las dos fechas por parámetro y retorna la diferencia de años entre una y otra 
+(edad del usuario).
  */
 package guia9ej04;
 
+import Service.FechaService;
+
 /**
  *
- * @author USUARIO
+ * @author The Wolf Legendary
  */
 public class Guia9Ej04 {
 
@@ -15,7 +22,8 @@ public class Guia9Ej04 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        FechaService fecha= new FechaService();
+        System.out.println("Usted tiene "+fecha.diferencia(fecha.fechaActual(), fecha.fechaNacimiento())+" años.");  
     }
     
 }
