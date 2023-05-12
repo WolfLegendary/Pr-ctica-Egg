065e7afdf3b3cd39e7bf3992b6e97d71a73f82e0
Federico Tula <fede.t17@hotmail.com>
2023-05-12 20:53:17 -0300

    Se crea Guia 10 Ej06

---
+++ Guia 10 Ej06/src/Entidad/Producto.java
@@ -0,0 +1,50 @@
+/*
+Se necesita una aplicación para una tienda en la cual queremos almacenar los distintos
+productos que venderemos y el precio que tendrán. Además, se necesita que la
+aplicación cuente con las funciones básicas.
+Estas las realizaremos en el servicio. Como, introducir un elemento, modificar su precio,
+eliminar un producto y mostrar los productos que tenemos con su precio (Utilizar
+Hashmap). El HashMap tendrá de llave el nombre del producto y de valor el precio.
+Realizar un menú para lograr todas las acciones previamente mencionadas.
+ */
+package Entidad;
+
+/**
+ *
+ * @author The Wolf Legendary
+ */
+public class Producto {
+
+    //Atributos
+    private String nombre;
+    private Double precio;
+
+    public Producto() {
+    }
+
+    public Producto(String nombre, Double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" + "nombre=" + nombre + ", precio=" + precio + '}';
+    }
+}
