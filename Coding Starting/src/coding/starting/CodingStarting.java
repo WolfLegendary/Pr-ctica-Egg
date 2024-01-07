cd563c6e5fe3381b534aad38998233d96e017891
Federico Tula <fede.t17@hotmail.com>
2024-01-07 13:25:29 -0300

    Start coding

--- Coding Starting/src/coding/starting/CodingStarting.java
+++ Coding Starting/src/coding/starting/CodingStarting.java
@@ -10,13 +10,14 @@ public class CodingStarting {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-          // Ejemplo de uso
+        // Ejemplo de uso
         String[] words1 = {"racecar", "noon", "civic"};
         String[] words2 = {"racecar", "shoe", "moon"};
 
         System.out.println("Palabras 1: " + checkAllPalindromes(words1)); // Debería imprimir true
         System.out.println("Palabras 2: " + checkAllPalindromes(words2)); // Debería imprimir false
     }
+
     public static String reverseWord(String word) {
         String reversed = "";
         for (int i = word.length() - 1; i >= 0; i--) {
